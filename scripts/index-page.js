267b1822ae5929a73247083a46789e924f757c94
Olusanya Jegede <olusanyajegede1@gmail.com>
2023-10-04 20:26:53 +0100

    feat: Add comment section structure and styling on bio page

---
+++ scripts/index-page.js
@@ -0,0 +1,30 @@
+/* create an array of comments with author, title, date 
+and image as and object
+*/
+
+// query get the commetns section
+// apend the comments post to the section above
+
+// create a for loop to check the comments lentgh
+// create an elemtns for the other comments post to be held
+// add a class to the element created above
+
+// create user post Element
+// add class to the above
+// titleEL.innerTxt = comments[i].title;
+// append title to Comment Element
+
+// create date post Element
+// add class to the above
+// dateEL.innerTxt = comments[i].date;
+// append date to Comment Element
+
+// create author Element
+// add class to the above
+// authorEL.innerTxt = comments[i].author;
+// append author to Comment Element
+
+// create img Element
+// add class to the above
+// imgEL.innerTxt = comments[i].img;
+// append img to Comment section
